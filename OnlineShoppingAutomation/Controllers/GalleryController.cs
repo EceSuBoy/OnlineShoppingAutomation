3d101b6c7cbdc303938ea43c926ba9705f2167be
Ece Su Boy <boyecesu@gmail.com>
2025-08-13 08:59:03 +0300

    Gallery operations

---
+++ OnlineShoppingAutomation/Controllers/GalleryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineShoppingAutomation.Models.Classes;
+
+namespace OnlineShoppingAutomation.Controllers
+{
+    public class GalleryController : Controller
+    {
+        Context c = new Context();
+        // GET: Gallery
+        public ActionResult Index()
+        {
+            var degerler = c.Products.ToList();
+            return View(degerler);
+        }
+    }
+}
